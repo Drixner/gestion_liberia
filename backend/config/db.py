3b3e2719b5dec5c4348377a97a59172b1a74e254
Drixner <drixnerrafael1@gmail.com>
2024-02-04 01:26:32 -0500

    Update database connection URL to use 'localhost' instead of '127.0.0.1'

--- backend/config/db.py
+++ backend/config/db.py
@@ -14,7 +14,7 @@ password = os.getenv("DB_PASS")
 database_name = os.getenv("DB_NAME")
 
 SQLALCHEMY_DATABASE_URL = (
-    f"mysql+mysqlconnector://{user}:{password}@127.0.0.1/{database_name}"
+    f"mysql+mysqlconnector://{user}:{password}@localhost/{database_name}"
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
